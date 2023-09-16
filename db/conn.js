f6fe3c713b27a0fbaa5bb0446c4e02c636b9fdcf
tharinduabeywickrama <a.tharindu@gtngroup.com>
2023-09-16 19:17:23 +0530

    Removed node_modules folder

--- db/conn.js
+++ db/conn.js
@@ -14,7 +14,8 @@ module.exports = {
       // Verify we got a good "db" object
       if (db)
       {
-        _db = db.db("employees");
+        // _db = db.db("employees");
+        _db = db.db("mydoc");
         console.log("Successfully connected to MongoDB."); 
       }
       return callback(err);
