abb4dbae66141a9168eb63df3e2fcf1c9654fe21
tharinduabeywickrama <a.tharindu@gtngroup.com>
2023-10-22 22:36:13 +0530

    Form enhancements

--- models/form.js
+++ models/form.js
@@ -7,17 +7,37 @@ const FormSchema = new Schema({
         type: Schema.Types.String,
         require: true,
     },
-    address: {
-        type: Schema.Types.String,
-        require: true,
-    },
-    contact: {
-        type: Schema.Types.String,
-        require: true,
-    },
+    // address: {
+    //     type: Schema.Types.String,
+    //     require: true,
+    // },
+    // contact: {
+    //     type: Schema.Types.String,
+    //     require: true,
+    // },
     childFormRef: {
         type: Schema.Types.String,
         require: true,
+    },
+    label: {
+        type: Schema.Types.String,
+        require: true,
+    },
+    type: {
+        type: Schema.Types.String,
+        require: true,
+    },
+    required: {
+        type: Schema.Types.String,
+        require: true,
+    },
+    key: {
+        type: Schema.Types.String,
+        require: true,
+    },
+    bold: {
+        type: Schema.Types.String,
+        require: false,
     }
 }, {collection : 'Form'});
 
