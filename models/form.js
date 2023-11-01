f9298623efe7a3b88a100e0ed0cb0447b4df7274
tharinduabeywickrama <a.tharindu@gtngroup.com>
2023-11-01 23:23:12 +0530

    Form save related issue fixes

--- models/form.js
+++ models/form.js
@@ -3,42 +3,39 @@ const Schema = mongoose.Schema;
 const conn = require('../config/db-connect');
 
 const FormSchema = new Schema({
-    name: {
-        type: Schema.Types.String,
+    fields: {
+        type: Schema.Types.Array,
         require: true,
     },
-    // address: {
-    //     type: Schema.Types.String,
-    //     require: true,
-    // },
-    // contact: {
-    //     type: Schema.Types.String,
-    //     require: true,
-    // },
     childFormRef: {
         type: Schema.Types.String,
         require: true,
-    },
-    label: {
-        type: Schema.Types.String,
-        require: true,
-    },
-    type: {
-        type: Schema.Types.String,
-        require: true,
-    },
-    required: {
-        type: Schema.Types.String,
-        require: true,
-    },
-    key: {
-        type: Schema.Types.String,
-        require: true,
-    },
-    bold: {
-        type: Schema.Types.String,
-        require: false,
     }
+    // name: {
+    //     type: Schema.Types.String,
+    //     require: true,
+    // },
+    
+    // label: {
+    //     type: Schema.Types.String,
+    //     require: true,
+    // },
+    // type: {
+    //     type: Schema.Types.String,
+    //     require: true,
+    // },
+    // required: {
+    //     type: Schema.Types.String,
+    //     require: true,
+    // },
+    // key: {
+    //     type: Schema.Types.String,
+    //     require: true,
+    // },
+    // bold: {
+    //     type: Schema.Types.String,
+    //     require: false,
+    // }
 }, {collection : 'Form'});
 
 module.exports = Form = conn.myDocDB.model('Form', FormSchema);
