eb38e4f08d050d475344d64911b067d7d0a067bb
tharinduabeywickrama <a.tharindu@gtngroup.com>
2023-11-04 16:45:52 +0530

    Form enhancments

--- models/form.js
+++ models/form.js
@@ -1,11 +1,22 @@
 const mongoose = require('mongoose');
-const fs = require('fs');
-const formPath = 'resources/default-form.json';
+// const fs = require('fs');
+// const formPath = 'resources/default-form.json';
 const Schema = mongoose.Schema;
 const conn = require('../config/db-connect');
 const formStatus = require('../constants/form-status');
 
 const FormSchema = new Schema({
+    formId: {
+        type: Schema.Types.String,
+        require: true,
+        unique: true,
+    },
+    color: {
+        type: Schema.Types.String,
+    },
+    formType: {
+        type: Schema.Types.String,   
+    },
     fields: {
         type: Schema.Types.Array,
         require: true,
@@ -22,24 +33,26 @@ const FormSchema = new Schema({
 
 const Form = conn.myDocDB.model('Form', FormSchema);
 
-fs.readFile(formPath, 'utf8', async (err, file) => {
+/* Below line is commented temporarly */
 
-    // check for any errors
-      if (err) {
-        console.error('Error while reading the file:', err)
-        return;
-      }
-      try {
-        const data = JSON.parse(file);
-        const allForms = await Form.find();
-        if(allForms && allForms.length === 0) {
-            const FormData = Form(data);
-            const savedData = await FormData.save();
-            console.log('Default form data have been saved to the DB');
-        }
-      } catch (err) {
-        console.error('Error while parsing JSON data:', err)
-      }
-});
+// fs.readFile(formPath, 'utf8', async (err, file) => {
+
+//     // check for any errors
+//       if (err) {
+//         console.error('Error while reading the file:', err)
+//         return;
+//       }
+//       try {
+//         const data = JSON.parse(file);
+//         const allForms = await Form.find();
+//         if(allForms && allForms.length === 0) {
+//             const FormData = Form(data);
+//             const savedData = await FormData.save();
+//             console.log('Default form data have been saved to the DB');
+//         }
+//       } catch (err) {
+//         console.error('Error while parsing JSON data:', err)
+//       }
+// });
 
 module.exports = Form;
