1a93d73b8b67a954e98114ce8909d11af3434f5b
tharinduabeywickrama <a.tharindu@gtngroup.com>
2023-11-23 23:15:13 +0530

    Initial form data saving on startup changes

--- models/form.js
+++ models/form.js
@@ -1,15 +1,14 @@
 const mongoose = require('mongoose');
-// const fs = require('fs');
-// const formPath = 'resources/default-form.json';
 const Schema = mongoose.Schema;
 const conn = require('../config/db-connect');
 const formStatus = require('../constants/form-status');
+const initialDbSave = require('../services/initial-db-save');
 
 const FormSchema = new Schema({
     formId: {
         type: Schema.Types.String,
-        require: true,
-        unique: true,
+        require: false,
+        // unique: true,
     },
     color: {
         type: Schema.Types.String,
@@ -33,26 +32,6 @@ const FormSchema = new Schema({
 
 const Form = conn.myDocDB.model('Form', FormSchema);
 
-/* Below line is commented temporarly */
-
-// fs.readFile(formPath, 'utf8', async (err, file) => {
-
-//     // check for any errors
-//       if (err) {
-//         console.error('Error while reading the file:', err)
-//         return;
-//       }
-//       try {
-//         const data = JSON.parse(file);
-//         const allForms = await Form.find();
-//         if(allForms && allForms.length === 0) {
-//             const FormData = Form(data);
-//             const savedData = await FormData.save();
-//             console.log('Default form data have been saved to the DB');
-//         }
-//       } catch (err) {
-//         console.error('Error while parsing JSON data:', err)
-//       }
-// });
+initialDbSave.insertInitialFormData(Form);
 
 module.exports = Form;
---
+++ services/initial-db-save.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const formPath = 'resources/default-form.json';
+
+const insertInitialFormData = async (form) => {
+    console.log('initial data insert');
+    const allForms = await form.find();
+    if (allForms && allForms.length > 0) {
+        console.log('Initial form data already exists');
+        return;
+    }
+    fs.readFile(formPath, 'utf8', async (err, file) => {
+
+    // check for any errors
+      if (err) {
+        console.error('Error while reading the file:', err)
+        return;
+      }
+      try {
+        const formList = JSON.parse(file);
+        if(allForms && allForms.length === 0) {
+            console.log('Inserting initial form data');
+            formList.forEach(async formDat => {
+                try {
+                    const FormData = form(formDat);
+                    const savedData = await FormData.save();
+                    console.log('Default form data has been saved to the DB');
+                } catch (e) {
+                    console.error('Error while saving initial forms data:', e);
+                }
+            });
+        }
+      } catch (err) {
+        console.error('Error while parsing JSON data:', err)
+      }
+});
+};
+
+module.exports = {
+    insertInitialFormData,
+}
