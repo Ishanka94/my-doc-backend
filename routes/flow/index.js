af1ea990ca5898710cc1dd999a1749573acbd83d
tharinduabeywickrama <a.tharindu@gtngroup.com>
2023-11-30 21:48:23 +0530

    Flow auth validation removed

--- routes/flow/index.js
+++ routes/flow/index.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const controller = require('./controller');
 const authenticateToken = require('../../middleware/authenticate-token');
 
-router.get('/get-all-flows', authenticateToken, controller.getAllFlows);
+// router.get('/get-all-flows', authenticateToken, controller.getAllFlows);
+router.get('/get-all-flows', controller.getAllFlows);
 router.post('/create-flow', controller.createFlow);
 
 module.exports = router;
